0da154bd96bc7648d1c0824253cdd6414960ee4d
Bodil Stokke <bodil@bodil.org>
2014-09-13 12:24:18 -0500

    JQUERIFY

--- notes.js
+++ notes.js
@@ -1,28 +1,72 @@
+// Filter & map array
+ponies
+.filter((pony) => /e$/.test(pony))
+.map((pony) => "I love " + pony)
+.log();
+
+// Interval ticks
+Rx.Observable.interval(500).log();
+
+// Yield array in 500ms ticks
+Rx.Observable.zip(
+  Rx.Observable.interval(500),
+  ponies,
+  (tick, i) => i
+).log();
+
+
 // --- Actual game:
 
-function makeElement(node) {
-  return React.DOM.div({
-    className: node.id,
-    style: {
+const canvas = $("#canvas");
+
+$("&lt;div id='ground'>&lt;/div>").appendTo(canvas);
+$("&lt;div id='pinkie'>&lt;/div>").appendTo(canvas);
+$("&lt;div id='coin'>&lt;/div>").appendTo(canvas);
+$("&lt;div id='hater'>&lt;/div>").appendTo(canvas);
+
+function extend(target, src) {
+  for (let prop in src) target[prop] = src[prop];
+}
+
+function assoc() {
+  let out = {};
+  for (let i = 0; i < arguments.length; i++) {
+    extend(out, arguments[i]);
+  }
+  return out;
+}
+
+function onscreen(node) {
+  return !(node.x < -300 || node.y < -1000 || node.y > 1000);
+}
+
+function bindKey(key) {
+  let sub = new Rx.Subject();
+  Mousetrap.bind(key, () => {
+    sub.onNext(key);
+  });
+  return sub;
+}
+
+function updateElement(node) {
+  $(node.id)
+    .attr("class", node.classes || "")
+    .css({
       left: (node.x + (node.baseX || 0)) | 0 + "px",
       top: (node.y + (node.baseY || 0)) | 0 + "px"
-    }
-  });
+    });
 }
 
 function renderScene(nodes) {
-  return React.renderComponent(
-    React.DOM.div(null, nodes.map(makeElement)),
-    canvas
-  );
+  nodes.forEach(updateElement);
 }
 
 let groundStream = Rx.Observable.interval(33)
-      .map((x) => ({
-    id: "ground",
+  .map((x) => ({
+    id: "#ground",
     baseX: -128,
     x: ((x % 64) * -8), y: 384
-      }));
+  }));
 
 function velocity(node) {
   return assoc(node, {
@@ -35,7 +79,7 @@ let tick = bindKey("space")
       .buffer(Rx.Observable.interval(33));
 
 let initialHater = {
-  id: "hater",
+  id: "#hater",
   vx: -8, vy: 0,
   x: 1600, y: 300
 };
@@ -47,7 +91,7 @@ let haterStream = tick.scan(initialHater, (c, keys) => {
 });
 
 let pinkieStream = Rx.Observable.zipArray(tick, haterStream).scan({
-  id: "pinkie",
+  id: "#pinkie",
   baseY: 276,
   x: 0, y: 0,
   vx: 0, vy: 0,
@@ -57,7 +101,7 @@ let pinkieStream = Rx.Observable.zipArray(tick, haterStream).scan({
 
   if (intersects(p, hater) && !p.gameOver) {
     p.gameOver = true;
-    p.id = "pinkie gameover";
+    p.classes = "gameover";
     new Audio("sfx/gameover.mp3").play();
     p.vy = -15;
   }
@@ -86,13 +130,13 @@ let pinkieStream = Rx.Observable.zipArray(tick, haterStream).scan({
     }
   }
 
-  p.id = (p.y < 0) ? "pinkie jumping" : "pinkie";
+  p.classes = (p.y < 0) ? "jumping" : "";
 
   return p;
 }).takeWhile(onscreen);
 
 let initialCoin = {
-  id: "coin",
+  id: "#coin",
   vx: -6, vy: 0,
   x: 1600, y: 40
 };
